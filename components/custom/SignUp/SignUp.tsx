88fd0b4ff0ec89e2fac1a6ee49b553f48eb16f41
anna <anka-kudrow@mail.ru>
2025-04-11 07:55:56 +0300

    change next.config

--- components/custom/SignUp/SignUp.tsx
+++ components/custom/SignUp/SignUp.tsx
@@ -5,7 +5,9 @@ export const SignUp = () => {
   return (
     <section className="sign-up">
       <div className="sign-up__inner">
-        <h2 className="sign-up__title title">Заполни анкету !</h2>
+        <h2 id="sign-form" className="sign-up__title title">
+          Заполни анкету !
+        </h2>
         <p className="sign-up__text">Начни преображение уже сегодня</p>
         <ClientForm />
       </div>
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
    images: {
     domains: ['images.ctfassets.net'],
+    unoptimized: true,
+
   },
 };
 

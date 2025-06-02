2edb9604d8c3f8d5b73049b7703f85f0f724b74c
anna <anka-kudrow@mail.ru>
2025-06-03 00:03:51 +0300

    remove basePath and assetPrefix for Vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,24 @@
 import type { NextConfig } from "next";
 
+// const isProd = process.env.NODE_ENV === "production";
+
+// const repoName = "beauty-studio-next"; 
+
+// const nextConfig: NextConfig = {
+//   basePath: isProd ? `/${repoName}` : "",
+//   assetPrefix: isProd ? `/${repoName}` : "",
+//    images: {
+//     domains: ['images.ctfassets.net'],
+//   },
+// };
+
+// export default nextConfig;
 const isProd = process.env.NODE_ENV === "production";
 
-const repoName = "beauty-studio-next"; 
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}` : "",
-   images: {
+  basePath: "",
+  assetPrefix: "",
+  images: {
     domains: ['images.ctfassets.net'],
   },
 };
-
-export default nextConfig;

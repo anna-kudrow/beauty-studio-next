f5b1619cbcb3326c899f263b25ba20e20d70d696
anna <anka-kudrow@mail.ru>
2025-08-01 15:04:24 +0300

    add @next/bundle-analyzer

--- next.config.ts
+++ next.config.ts
@@ -6,4 +6,10 @@ const nextConfig: NextConfig = {
   },
 };
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+ 
+module.exports = withBundleAnalyzer(nextConfig)
+
 export default nextConfig;

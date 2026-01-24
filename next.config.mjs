f2e1fa5f04db2cdd822e2c172dea58c3d7f32aa1
anna <anka-kudrow@mail.ru>
2026-01-24 18:05:32 +0300

    change next config file

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.ctfassets.net"],
+  },
+};
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withBundleAnalyzer(nextConfig);
